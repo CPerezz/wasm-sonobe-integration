6bb481d4389463937156717e0fd4399c67701d35
CPerezz <c.perezbaro@gmail.com>
2024-10-15 18:24:37 +0200

    update: Circom circuit compilation scripts

--- compile_circuits.sh
+++ compile_circuits.sh
@@ -1,1 +1,15 @@
-circom ./circuits/with_external_inputs.circom --r1cs --sym --wasm --prime bn128 --output ./
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Compile the circuit and get `r1cs` and `wasm` outputs.
+circom ./circuits/with_external_inputs.circom --r1cs --wasm --prime bn128 --output ./
+
+# Move `r1cs` and `wasm` outpus to `./public` to be served with our site statically.
+mv ./with_external_inputs/with_external_inputs.wasm ./public
+mv with_external_inputs.r1cs ./public
+
+# Remove the folder.
+rm -rf ./with_external_inputs
+
